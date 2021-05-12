1ffa4876181602ccbb95da5d215b511c2900d147
ERIK <2283984853@qq.com>
2021-05-12 23:52:46 +0800

    Add background image.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import background from '../asset/background.png'
 
 function App() {
   return (
     <div className="App">
+      <div style={{ 
+      backgroundImage: background 
+    }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
+      </div>
     </div>
   );
 }
