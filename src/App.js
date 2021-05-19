3544be9e05a8f4bc935de98891519f0b6ff74479
ERIKQQY <2283984853@qq.com>
2021-05-19 23:53:56 +0800

    Fix CI failing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import background from './background.jpg'
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-import { makeStyles } from '@material-ui/core/styles';
 
 function App() {
   return (
